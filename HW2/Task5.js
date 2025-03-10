7d5184be4a831232349260bd330156a1e40ef35c
Oleksandra Zapara <aleksandra.zapar@gmail.com>
2025-03-10 15:36:44 +0200

    Updates

--- HW2/Task5.js
+++ HW2/Task5.js
@@ -1,15 +1,13 @@
 function checkID() { 
-    let idInput = Number(prompt("Enter your ID between 1 and 1000"));
-if (idInput < 1 || idInput > 1000) {
- throw new Error("Please enter your ID between 1 and 1000");
-}
-if (isNaN(idInput)) {
-    throw new Error("Invalid ID. Please enter a number");  
-}
-if (idInput === null || idInput.trim() === ""){
-    throw new Error("ID cannot be empty. Please enter your ID"); 
-}
- alert(idInput + ": " + "Your ID has been passed verification");
+    let idInput = prompt("Enter your ID between 1 and 1000");
+    
+    if (idInput.trim() === ""){
+        throw new Error("ID cannot be empty. Please enter your ID");
+    }else if (isNaN(idInput)) {
+        throw new Error("Invalid ID. Please enter a number");  
+    }else if (idInput < 1 || idInput > 1000) {
+        throw new Error("Please enter your ID between 1 and 1000");
+} alert(idInput + ": " + "Your ID has been passed verification");
 }
 
 try {
@@ -17,4 +15,5 @@ try {
 } catch (error) {
 console.log(error.name);
 console.log(error.message);
+alert(error.message);
 }
