dbd565a08dbb36df63b3557959ac044a56f3c9f7
Oleksandra Zapara <aleksandra.zapar@gmail.com>
2025-03-12 17:49:34 +0200

    added Task4 part2

---
+++ HW4/Task1.js
@@ -0,0 +1,10 @@
+const scores = {
+    programming: 88,
+    design: 74,
+    jurisprudence: false,
+    relational_DB: 92,
+    computerGraphics: "submitted",
+    patterns: 65,
+    philosophy: "transferred",
+    network: 81
+  };
